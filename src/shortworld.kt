fba6a4c19e46457478d72eb688ffb590a756ba90
aperte <aperte@ctrlz.info>
2019-01-12 01:02:43 +0100

    exploring kotlin (String extension and iterators)

--- src/shortworld.kt
+++ src/shortworld.kt
@@ -2,4 +2,29 @@ fun main(args: Array<String>) {
     val msg = "Hello, world! We digress."
 
     println(msg)
+
+    println(msg.remake())
+
+    val rotted = msg.rot13()
+    println(rotted)
+    println(rotted.derot())
+}
+
+fun String.remake(): String {
+    val str = this.map { it.toByte().toChar() }
+    return str.joinToString("")
+}
+
+fun String.rot13(): String {
+    return this.map {
+        val byte: Byte = (it.toByte() + 13.toByte()).toByte()
+        byte.toChar()
+    }.joinToString ( "" )
+}
+
+fun String.derot(): String {
+    return this.map {
+        val byte: Byte = (it.toByte() - 13.toByte()).toByte()
+        byte.toChar()
+    }.joinToString("")
 }
