cfe95de4249d1613ba201b81e5746f29934c2067
aperte <aperte@ctrlz.info>
2019-01-13 02:58:12 +0100

    shared variables between shaders

--- src/shortworld.kt
+++ src/shortworld.kt
@@ -116,15 +116,22 @@ precision highp float;
 
 uniform vec4 uColor;
 
+varying vec2 vertPos;
+
 void main() {
-    gl_FragColor = uColor;
+    vec4 col = uColor + vertPos.y;
+    gl_FragColor = col;
 }
 """
 
 private val vertexShader = """
 attribute vec2 aPos;
 
+varying vec2 vertPos;
+
 void main() {
-    gl_Position = vec4(aPos, 0.0, 1.0);
+    vec2 vPos = aPos;
+    gl_Position = vec4(vPos, 0.0, 1.0);
+    vertPos = vPos;
 }
 """
