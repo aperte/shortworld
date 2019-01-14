80869e3beb488139e9a62126bf33c6e9c750f62e
aperte <aperte@ctrlz.info>
2019-01-14 20:31:46 +0100

    early prototypes for:
    * click handling (currently draws a circle)
    * keyboard handling (moves viewport)
    * matrix instead of resolution (current inactive)
    * moved into using pixels instead of pure clipspace
    * prototype for debug property tracking (lists live properties)
    
    next commit contains the added math library.

--- src/shaders.kt
+++ src/shaders.kt
@@ -5,22 +5,40 @@ precision highp float;
 
 uniform vec4 uColor;
 
-varying vec2 vertPos;
+varying vec2 sPos;
 
 void main() {
-    vec4 col = uColor + vertPos.y;
-    gl_FragColor = col;
+    gl_FragColor = uColor;
 }
 """
 
 const val vertexShader = """
-attribute vec2 aPos;
+attribute vec2 aVal;
 
-varying vec2 vertPos;
+varying vec2 sPos;
 
 void main() {
-    vec2 vPos = aPos;
+    vec2 vPos = aVal;
     gl_Position = vec4(vPos, 0.0, 1.0);
-    vertPos = vPos;
+    sPos = vPos;
+}
+"""
+
+const val vertexShaderMat = """
+attribute vec2 aVal;
+
+varying vec2 sPos;
+
+uniform vec2 uResolution;
+uniform mat4 uPerspective;
+uniform mat4 uModel;
+
+void main() {
+    vec2 zeroToOne = aVal / uResolution;
+    vec2 zeroToTwo = zeroToOne * 2.0;
+    vec2 clipSpace = zeroToTwo - 1.0;
+    vec4 final = vec4(clipSpace * vec2(1, -1), 0.0, 1.0);
+    gl_Position = final;
+    sPos = vec2(final.x, final.y);
 }
 """
--- src/shortworld.kt
+++ src/shortworld.kt
@@ -1,6 +1,13 @@
+import extra.math.*
 import org.khronos.webgl.*
 import org.khronos.webgl.WebGLRenderingContext as GL
 import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.HTMLLIElement
+import org.w3c.dom.HTMLOListElement
+import org.w3c.dom.HTMLUListElement
+import org.w3c.dom.events.Event
+import org.w3c.dom.events.KeyboardEvent
+import org.w3c.dom.events.MouseEvent
 import kotlin.browser.document
 import kotlin.browser.window
 import kotlin.js.Date.Companion.now
@@ -35,32 +42,81 @@ class RingBuffer2<T>(capacity: Int, init: (Int) -> T) {
     }
 }
 
+class PropertyTracker(val id: String, val updater: () -> String, owner: HTMLUListElement) {
+    val element = document.createElement("li") as HTMLLIElement
+
+    init {
+        owner.appendChild(element)
+    }
+
+    fun update() {
+        element.innerHTML = "$id=${updater()}"
+    }
+}
+
 class Manager {
     val canvas = document.getElementById("glCanvas") as HTMLCanvasElement
     val gl = canvas.getContext("webgl") as GL
+    val propertyList = document.getElementById("entries") as HTMLUListElement
+    val properties: MutableList<PropertyTracker> = mutableListOf()
     var program: WebGLProgram
+    val elements: MutableList<Circle> = mutableListOf()
 
     init {
-        program = createProgram(gl, fragmentShader, vertexShader)
+        program = createProgram(gl, fragmentShader, vertexShaderMat)
+        canvas.addEventListener("click", ::click_handler)
+        canvas.addEventListener("keydown", ::keydown_handler)
+        canvas.setAttribute("tabindex", "0")
+        canvas.focus()
+    }
+
+    fun eventHandler(event: Event) {
+        println("EVENT: ${event.type}")
+    }
+
+    fun click_handler(event: Event) {
+        val mevent = event as MouseEvent
+        val offsetLeft = canvas.offsetLeft
+        val offsetTop = canvas.offsetTop
+        val clientX = mevent.clientX - offsetLeft
+        val clientY = mevent.clientY - offsetTop
+        println("clientX=${clientX}")
+        println("clientY=${clientY}")
+
+        val new_circle = Circle(gl, program, 50f, 36, offsetX = clientX.toFloat(), offsetY = clientY.toFloat())
+        elements.add(new_circle)
+    }
+
+    fun keydown_handler(event: Event) {
+        val kevent = event as KeyboardEvent
+        when (kevent.keyCode) {
+            87 -> viewportY += 50
+            83 -> viewportY -= 50
+            65 -> viewportX -= 50
+            68 -> viewportX += 50
+            else -> {
+                println("KEYDOWN=${kevent.keyCode} (${kevent.key})")
+            }
+        }
     }
 
     var ticks = 0L
     var last_second = 0L
-    val circle = Circle(gl, program, 1.0f, 36)
 
     var elapsed: Double = 0.0
     var seconds = 0
     var elapsed_buffer = RingBuffer2(5, { 0L })
     var last = now()
 
+    var viewportX = 0
+    var viewportY = 0
+
     fun driver() {
         elapsed += now() - last // count time outside our control as well
         val start = now()
-        if ((ticks % 1000) == 0L) { // every 1000 ticks
-            val tpsec = ticks / max(seconds, 1)
-            println("tick=$ticks, ticks_per_second=$tpsec")
-            val averaged = elapsed_buffer.values().sum().div(5)
-            println("average_ticks_per_last_5_seconds=$averaged")
+
+        for (tracker in properties) {
+            tracker.update()
         }
 
         logic()
@@ -70,7 +126,7 @@ class Manager {
         elapsed += now() - start
         if (elapsed.toInt() > 1000) {
             elapsed_buffer.add(ticks - last_second)
-            seconds++
+            seconds++ // note: this will skip seconds that have elapsed when browser tab was inactive, we also do not keep remainders making it skip seconds eventually
             elapsed = 0.0
             last_second = ticks
         }
@@ -78,18 +134,75 @@ class Manager {
         window.requestAnimationFrame { driver() }
     }
 
+    val circle = Circle(gl, program, 100f, 36)
+    var width = canvas.clientWidth.toFloat()
+    var height = canvas.clientHeight.toFloat()
+    val model = Mat4.identity()
+    val perspective = Mat4.identity()
+
+    fun sortho(width: Float, height: Float, depth: Float): Mat4 {
+        return Mat4(
+                Float4(x = 2f / width),
+                Float4(y = -2f / height),
+                Float4(z = 2 / depth),
+                Float4(x = -1f, y = 1f, z = 0f, w = 1f)
+        )
+    }
+
+    fun resize() {
+        if (height.toInt() != canvas.clientHeight) {
+            height = canvas.clientHeight.toFloat()
+            canvas.height = height.toInt()
+        }
+        if (width.toInt() != canvas.clientWidth) {
+            width = canvas.clientWidth.toFloat()
+            canvas.width = width.toInt()
+        }
+        gl.viewport(viewportX, viewportY, width.toInt(), height.toInt())
+    }
+
+    init {
+        listOf(
+                PropertyTracker("ticks", { "$ticks" }, propertyList),
+                PropertyTracker("avg_ticks(5sec)", { elapsed_buffer.values().sum().div(5).toString() }, propertyList),
+                PropertyTracker("viewport(X,Y)", { "($viewportX,$viewportY)" }, propertyList),
+                PropertyTracker("canvas.(cW,cH)", {
+                    "(${canvas.clientWidth.toString()},${canvas.clientHeight.toString()})"
+                }, propertyList),
+                PropertyTracker("canvas.(W,H)", {
+                    "(${canvas.width.toString()},${canvas.height.toString()})"
+                }, propertyList),
+                PropertyTracker("(W,H)", { "($width,$height)"}, propertyList)
+        ).forEach { properties.add(it) }
+        canvas.height = height.toInt()
+        canvas.width = width.toInt()
+        println("dpr=${window.devicePixelRatio.toFloat()}")
+    }
+
+    val uPerspective = gl.getUniformLocation(program, "uPerspective")
+    val uModel = gl.getUniformLocation(program, "uModel")
+    val uResolution = gl.getUniformLocation(program, "uResolution")
+    val uColor = gl.getUniformLocation(program, "uColor")
+
     fun render() {
-        gl.viewport(0, 0, canvas.width, canvas.height)
+        resize()
+        //val perspective = ortho(0f, width, height, 0f, -1f, 1f)
         gl.clearColor(0f, 0f, 0f, 1f)
         gl.clear(GL.COLOR_BUFFER_BIT)
 
         gl.useProgram(program)
-        val uColor = gl.getUniformLocation(program, "uColor")
 
         gl.uniform4fv(uColor, arrayOf(0.7f, 0.7f, 0.2f, 1f))
 
-        //renderCircle(gl, program)
+        gl.uniform2fv(uResolution, arrayOf(width, height))
+        gl.uniform4fv(uColor, arrayOf(0.7f, 0.7f, 0.2f, 1f))
+        gl.uniformMatrix4fv(uPerspective, false, perspective.toFloatArray().toTypedArray())
+        gl.uniformMatrix4fv(uModel, false, model.toFloatArray().toTypedArray())
+
         circle.render()
+        for (ele in elements) {
+            ele.render()
+        }
         renderSquare(gl, program)
     }
 
@@ -106,9 +219,9 @@ fun renderSquare(gl: GL, program: WebGLProgram) {
     gl.bindBuffer(GL.ARRAY_BUFFER, vbuf)
     gl.bufferData(GL.ARRAY_BUFFER, square_sides, GL.STATIC_DRAW)
 
-    val aPos = gl.getAttribLocation(program, "aPos")
-    gl.enableVertexAttribArray(aPos)
-    gl.vertexAttribPointer(aPos, 2, GL.FLOAT, false, 0, 0)
+    val aVal = gl.getAttribLocation(program, "aVal")
+    gl.enableVertexAttribArray(aVal)
+    gl.vertexAttribPointer(aVal, 2, GL.FLOAT, false, 0, 0)
     gl.drawArrays(GL.TRIANGLES, 0, square_sides.length / 2)
 }
 
@@ -125,7 +238,7 @@ fun List<Vertex>.asArray(): Float32Array {
     return vertices
 }
 
-class Circle(val gl: GL, val program: WebGLProgram, radius: Float, segments: Int) {
+class Circle(val gl: GL, val program: WebGLProgram, radius: Float, segments: Int, offsetX: Float = 0f, offsetY: Float = 0f) {
     val bufname: WebGLBuffer
     val vertices: Int
 
@@ -134,14 +247,14 @@ class Circle(val gl: GL, val program: WebGLProgram, radius: Float, segments: Int
         while (360 % segmentsActual != 0) {
             segmentsActual += 1
         }
-        val startX = 0.0f * radius
-        val startY = 0.0f * radius
+        val startX = offsetX + 0.0f
+        val startY = offsetY + 0.0f
         val array = listOf(Vertex(startX, startY)) + // first prepend the initial vertex
                 (0..361)
                         .filter { it % (360 / segmentsActual) == 0 }
                         .map {
                             val rad: Float = it.toFloat() * (PI.toFloat() / 180)
-                            Vertex(cos(rad) * radius, sin(rad) * radius)
+                            Vertex(offsetX + (cos(rad) * radius), offsetY + (sin(rad) * radius))
                         }
         bufname = gl.createBuffer()!!
         gl.bindBuffer(GL.ARRAY_BUFFER, bufname)
@@ -152,9 +265,9 @@ class Circle(val gl: GL, val program: WebGLProgram, radius: Float, segments: Int
     fun render() {
         gl.bindBuffer(GL.ARRAY_BUFFER, bufname)
 
-        val aPos = gl.getAttribLocation(program, "aPos")
-        gl.enableVertexAttribArray(aPos)
-        gl.vertexAttribPointer(aPos, 2, GL.FLOAT, false, 0, 0)
+        val aVal = gl.getAttribLocation(program, "aVal")
+        gl.enableVertexAttribArray(aVal)
+        gl.vertexAttribPointer(aVal, 2, GL.FLOAT, false, 0, 0)
         gl.drawArrays(GL.TRIANGLE_FAN, 0, vertices)
     }
 }
@@ -182,9 +295,9 @@ fun renderCircle(gl: GL, program: WebGLProgram, radius: Float = 1.0f, segments: 
 
     gl.uniform4fv(uColor, arrayOf(0.7f, 0.7f, 0.2f, 1f))
 
-    val aPos = gl.getAttribLocation(program, "aPos")
-    gl.enableVertexAttribArray(aPos)
-    gl.vertexAttribPointer(aPos, 2, GL.FLOAT, false, 0, 0)
+    val aVal = gl.getAttribLocation(program, "aVal")
+    gl.enableVertexAttribArray(aVal)
+    gl.vertexAttribPointer(aVal, 2, GL.FLOAT, false, 0, 0)
     gl.drawArrays(GL.TRIANGLE_FAN, 0, vertices.size)
 }
 
